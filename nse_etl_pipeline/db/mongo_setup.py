0858eee85a51dd670075912223576985118c8759
Manoj <91754179+manojDOX@users.noreply.github.com>
2025-12-12 21:05:54 +0530

    Delete nse_etl_pipeline directory

--- nse_etl_pipeline/db/mongo_setup.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-def setup_mongo():
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.market_data
-    db.create_collection("upstox_nse")
-    client.close()
--- nse_etl_pipeline/db/sql_setup.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS dhan_nse (
-    exchange TEXT,
-    security_id TEXT,
-    symbol_name TEXT,
-    trading_symbol TEXT PRIMARY KEY
-);
